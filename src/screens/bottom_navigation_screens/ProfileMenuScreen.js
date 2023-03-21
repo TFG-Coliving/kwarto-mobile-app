c297251859998c323988a29091f9e59656046f2d
Alberto González Fernández <57061123+albertogonfer@users.noreply.github.com>
2023-03-21 10:47:06 +0100

    Revert "Feature/tfgdam2 10 creacion screen profile"

--- src/screens/bottom_navigation_screens/ProfileMenuScreen.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ProfileMenuNavigation from "../../navigation/ProfileStackNavigation";
-
-
-export default function ProfileMenuScreen() {
-  return (
-      <ProfileMenuNavigation/>
-  );
-}

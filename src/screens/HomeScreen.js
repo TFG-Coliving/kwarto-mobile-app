726968750a79ee03eaabf4b64348577479bd3b8e
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2023-03-23 19:27:19 +0100

    tutorial home terminado

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,26 +1,126 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
-import StackScreenButton from "../components/StackScreenButton";
-import {useNavigation} from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
 const HomeScreen = () => {
   const navigation = useNavigation();
-  return (
-    <View>
-      <Text
-          style={styles.text}
-      >Home Screen</Text>
-      <StackScreenButton onPress={() => navigation.navigate("StackScreen")}/>
-    </View>
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
 
+  useEffect(() => {
+    fetchData("https://randomuser.me/api/?results=20");
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLargeTitle: true,
+      headerTitle: "Home",
+      headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Stack")}
+            style={{
+              backgroundColor: "purple",
+              width: 30,
+              height: 30,
+              borderRadius: 10,
+              justifyContent: "center",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 20,
+                textAlign: "center",
+                color: "white",
+              }}
+            >
+              +
+            </Text>
+          </TouchableOpacity>
+    ),
+    headerSearchBarOptions: {
+      placeholder: "Friends",
+      onChangeText: (event) => {
+        searchFilterFunction(event.nativeEvent.text);
+      },
+    },
+    });
+  }, [navigation]);
+
+  const fetchData = async (url) => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      setData(json.results);
+      setFilterData(json.results);
+      console.log(json.results);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const searchFilterFunction = (text) => {
+    if (text){
+      const newData = data.filter(item => {
+        const itemData = item.name.first ? item.name.first.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      })
+      setFilterData(newData);
+    }else{
+      setFilterData(data);
+    }
+  }
+
+  return (
+    <ScrollView>
+      <Text style={styles.textFriends}>Friends</Text>
+      {filterData.map((item, index) => {
+        return (
+          <View key={index} style={styles.itemContainer}>
+            <Image source={{ uri: item.picture.large }} style={styles.image} />
+            <View>
+              <Text style={styles.textName}>
+                {item.name.first}
+                {item.name.last}
+              </Text>
+              <Text style={styles.textEmail}>{item.login.username}</Text>
+            </View>
+          </View>
+        );
+      })}
+    </ScrollView>
   );
-}
+};
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: "20%"
-  }
+  textFriends: {
+    fontSize: 20,
+    textAlign: "left",
+    marginLeft: 10,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItes: "center",
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  textName: {
+    fontSize: 17,
+    marginLeft: 10,
+    fontWeight: "600",
+  },
+  textEmail: {
+    fontSize: 14,
+    marginLeft: 10,
+    color: "grey",
+  },
 });

33220e21f0d8363d99632b7e8d978f118bf74b56
Javier Aguilera <javieraguileralabrador99@gmail.com>
2023-03-24 20:30:10 +0100

    Feature/Login-Register visual

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import BottomNavigation from './src/navigation/BottomNavigation';
+import LoginScreen from "./src/screens/login_register_screens/LoginScreen";
+import {NavigationContainer} from "@react-navigation/native";
+import AuthStack from "./src/navigation/AuthStack";
 
 
 export default function App() {
   return (
-    <BottomNavigation/>
+    <NavigationContainer>
+      <AuthStack/>
+    </NavigationContainer>
   );
 }
 

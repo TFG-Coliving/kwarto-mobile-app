a81269588e0791c9b8aeb7fa7f8d37332d34d34f
Alberto González Fernández <albertogonfer99@gmail.com>
2023-04-18 16:26:46 +0200

    Primera iteracion de personal info screen

--- src/screens/profile_screens/PersonalInfoScreen.js
+++ src/screens/profile_screens/PersonalInfoScreen.js
@@ -1,23 +1,57 @@
-import React from "react";
-import {View, Text, StyleSheet, TouchableOpacity} from "react-native";
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-const PersonalInfoScreen = ({navigation}) => {
+const PersonalInfoScreen = ({ navigation }) => {
   return (
-      <View>
-        <Text
-            style={styles.text}
-        >Personal Info</Text>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Personal Information</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.input} placeholder="Full Name" defaultValue={"Jhon Doe"} />
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Phone Number" />
+          <TextInput style={styles.input} placeholder="Address" />
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Save Changes</Text>
+        </TouchableOpacity>
       </View>
-
   );
 }
-
 export default PersonalInfoScreen;
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: "20%"
-  }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 40,
+  },
+  heading: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#8667f1',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });

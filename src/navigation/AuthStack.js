246b31e9f1e47e4a725abeb732367d3d8b6ec6fe
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2023-04-24 17:25:40 +0200

    a

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -12,6 +12,7 @@ const AuthStack = () => {
             <Stack.Screen name="Login" component={LoginScreen}/>
             <Stack.Screen name="Register" component={RegisterScreen}/>
             <Stack.Screen name="Home" component={HomeScreen}/>
+            <Stack.Screen name="Home" component={BottomNavigation}/>
         </Stack.Navigator>
     );
 };

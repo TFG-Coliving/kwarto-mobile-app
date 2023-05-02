cf7abdf265687213b6bb72d9b72e5ac7844f3798
Alberto González Fernández <albertogonfer99@gmail.com>
2023-05-02 15:41:35 +0200

    Eliminacion de root component innecesario

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import React from 'react-native';
-import RootComponent from "./RootComponent";
 import {Provider} from "react-redux";
 import store from "./src/redux/store/store";
 import {NavigationContainer} from "@react-navigation/native";
 import AuthStack from "./src/navigation/AuthStack";
-import BottomNavigation from "./src/navigation/BottomNavigation";
 
 export default function App() {
 

b92524f0a63c9dafc1aa3fff40a01cc81a395885
TheNaviam <ivan.aranda.martinez.11@gmail.com>
2023-05-18 16:53:28 +0200

    a

--- src/navigation/BottomNavigation.js
+++ src/navigation/BottomNavigation.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {NavigationContainer} from "@react-navigation/native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 //icons
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import ProfileMenuScreen from "../screens/bottom_navigation_screens/ProfileMenuScreen";
 import HomeMenuScreen from "../screens/bottom_navigation_screens/HomeMenuScreen";
 import ReservasMenuScreen from "../screens/bottom_navigation_screens/ReservasMenuScreen";
@@ -11,53 +11,51 @@ import { Ionicons } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    return (
-        <Tab.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-                tabBarActiveTintColor: '#8667f1',
-                }
-            }
-        >
-
-            <Tab.Screen
-                name="Home"
-                component={HomeMenuScreen}
-                options={{
-                  tabBarIcon: ({ color, size }) => (
-                    <Ionicons color={color} size={25} name="home"></Ionicons>
-                  ),
-                  headerShown: false
-                }}
-            />
-            <Tab.Screen
-                name="Mis reservas"
-                component={ReservasMenuScreen}
-                options={{
-                  tabBarIcon: ({ color, size }) => (
-                    <Ionicons color={color} size={25} name="calendar"></Ionicons>
-                  ),
-                  headerShown: true
-                }}
-            />
-            <Tab.Screen
-              name="Profile"
-              component={ProfileMenuScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons color={color} size={25} name="person"></Ionicons>
-                ),
-                headerShown: false
-              }}
-            />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: "#8667f1",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeMenuScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={25} name="home"></Ionicons>
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Mis reservas"
+        component={ReservasMenuScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={25} name="calendar"></Ionicons>
+          ),
+          headerShown: true,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileMenuScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons color={color} size={25} name="person"></Ionicons>
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
 
 export default function BottomNavigation() {
-    return (
-        <NavigationContainer independent={true}>
-            <MyTabs/>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer independent={true}>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
